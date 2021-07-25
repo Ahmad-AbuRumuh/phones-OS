2104db0e82c2becc6c2b2b74096a11ef4c67f47c
AhmadRomoh <>
2021-07-25 13:54:11 +0000

    function

--- app.js
+++ app.js
@@ -10,15 +10,23 @@ while(numOfvOS > 5 || numOfvOS < 1){
   numOfvOS = prompt("Please select from 1-5")
 }
 
+function AndroidImage(){
+   return"<img  src='https://2k7p22nx6oe213gsh48gkhoz-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/bigstock-Android-Logo-On-Digital-Tablet-79211521-900x490.jpg' width='180' height='100'     />"
+}
+
+function IOSImage(){
+   return"<img  src='https://read.os-cdn.com/wp-content/uploads/2020/01/%D9%86%D8%B8%D8%A7%D9%85-ios.jpg' width='180' height='100'     />"
+}
+
 if(favOS == "Android"){
 alert("Ok, welcome")
 for(var i = 1; i <= numOfvOS; i++){
 document.write("<div>" + "<h3>" + favOS+"</h3>"
-+ "<img  src='https://2k7p22nx6oe213gsh48gkhoz-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/bigstock-Android-Logo-On-Digital-Tablet-79211521-900x490.jpg' width='180' height='100'     />" + "</div>")}
++ document.write(AndroidImage()) + "</div>")}
 }
 else if (favOS =="IOS"){
 alert("Ok, welcome")
 for(var i = 1; i <= numOfvOS; i++){
 document.write("<div>" + "<h3>" + favOS+"</h3>"
-+ "<img  src='https://read.os-cdn.com/wp-content/uploads/2020/01/%D9%86%D8%B8%D8%A7%D9%85-ios.jpg' width='180' height='100'     />" + "</div>")}
++ document.write(IOSImage()) + "</div>")}
 }
